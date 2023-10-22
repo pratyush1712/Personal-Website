c7d440f1080a90ef85d15ae2dbef4686b481f06b
Pratyush Sudhakar <ps2245@cornell.edu>
2023-10-21 23:25:02 -0400

    footer should not change color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -129,7 +129,7 @@ export default function Footer() {
 								}}
 							>
 								<VscCheck fontSize="0.9rem" />
-								<Typography sx={{ ml: 0.5, mt: 0.1, fontSize: "0.6rem" }}>Prettier</Typography>
+								<Typography sx={{ ml: 0.5, mt: 0.1, fontSize: "0.6rem", color: "white" }}>Prettier</Typography>
 							</Box>
 
 							<Box
