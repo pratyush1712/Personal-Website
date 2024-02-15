2c393bde9c6a6395385ac7756a29639640b65d0e
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2024-02-15 16:28:03 -0500

    Update data.py
    Signed-off-by: Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>

--- spotify/data.py
+++ spotify/data.py
@@ -3,9 +3,9 @@ songs = [
     "https://open.spotify.com/track/5rAUZy2eDdegBxUVYxePK2?si=f57fef9d11c04921",
     "https://open.spotify.com/track/4RVwu0g32PAqgUiJoXsdF8?si=c4e9c7b69d8f4080",
     "https://open.spotify.com/track/2UREu1Y8CO4jXkbvqAtP7g?si=fe53e684746d4731",
-    "https://open.spotify.com/track/0AS63m1wHv9n4VVRizK6Hc?si=d70fb5e67f314b05",
     "https://open.spotify.com/track/73SpzrcaHk0RQPFP73vqVR?si=afd067059b444ae8",
     "https://open.spotify.com/track/2MmAYegcUty1DmG0Gw7AMh?si=ce84c8e6c7764a1f",
+    "https://open.spotify.com/track/0AS63m1wHv9n4VVRizK6Hc?si=d70fb5e67f314b05",
     "https://open.spotify.com/track/0yc6Gst2xkRu0eMLeRMGCX?si=cdd25b91418746ad",
     "https://open.spotify.com/track/3ZCTVFBt2Brf31RLEnCkWJ?si=f6c291b7d7314dbb",
 ]
