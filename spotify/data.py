d3d079f1fa14ae4f885b885d2d675603f5633101
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2024-02-15 16:24:40 -0500

    Update data.py
    Signed-off-by: Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>

--- spotify/data.py
+++ spotify/data.py
@@ -1,7 +1,7 @@
 songs = [
     "https://open.spotify.com/track/5GYbkDveRD2I8M5ZJ14hWn?si=4babdfa3d5a24bd9",
-    "https://open.spotify.com/track/4RVwu0g32PAqgUiJoXsdF8?si=c4e9c7b69d8f4080",
     "https://open.spotify.com/track/5rAUZy2eDdegBxUVYxePK2?si=f57fef9d11c04921",
+    "https://open.spotify.com/track/4RVwu0g32PAqgUiJoXsdF8?si=c4e9c7b69d8f4080",
     "https://open.spotify.com/track/2UREu1Y8CO4jXkbvqAtP7g?si=fe53e684746d4731",
     "https://open.spotify.com/track/0AS63m1wHv9n4VVRizK6Hc?si=d70fb5e67f314b05",
     "https://open.spotify.com/track/73SpzrcaHk0RQPFP73vqVR?si=afd067059b444ae8",
