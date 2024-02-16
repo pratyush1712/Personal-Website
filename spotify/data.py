83de19da3c34ac5dfa48b06357f79c9ed02b9763
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2024-02-16 00:03:36 -0500

    Update data.py
    Signed-off-by: Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>

--- spotify/data.py
+++ spotify/data.py
@@ -13,5 +13,16 @@ songs = [
     "https://open.spotify.com/track/4SuA85PaD8GFTPYwKNYDe1?si=cc19e466b89649fc",
     "https://open.spotify.com/track/5O2P9iiztwhomNh8xkR9lJ?si=7c94c4e6cd6845aa",
     "https://open.spotify.com/track/68XhCrBajUR64h6wv4eYYW?si=b5e9e237f299453c",
-    "https://open.spotify.com/track/6TGX01ExmdHyOG72fqUU3Q?si=420ea8fe1be74d1b"
+    "https://open.spotify.com/track/6TGX01ExmdHyOG72fqUU3Q?si=420ea8fe1be74d1b",
+
+
+    "https://open.spotify.com/track/5GYbkDveRD2I8M5ZJ14hWn?si=4babdfa3d5a24bd9",
+    "https://open.spotify.com/track/5rAUZy2eDdegBxUVYxePK2?si=f57fef9d11c04921",
+    "https://open.spotify.com/track/4RVwu0g32PAqgUiJoXsdF8?si=c4e9c7b69d8f4080",
+    "https://open.spotify.com/track/2UREu1Y8CO4jXkbvqAtP7g?si=fe53e684746d4731",
+    "https://open.spotify.com/track/4bvHCjknZC3evn7NthBZVt?si=41cb65b179824c06",
+    "https://open.spotify.com/track/73SpzrcaHk0RQPFP73vqVR?si=afd067059b444ae8",
+    "https://open.spotify.com/track/0AS63m1wHv9n4VVRizK6Hc?si=d70fb5e67f314b05",
+    "https://open.spotify.com/track/0yc6Gst2xkRu0eMLeRMGCX?si=cdd25b91418746ad",
+    "https://open.spotify.com/track/3ZCTVFBt2Brf31RLEnCkWJ?si=f6c291b7d7314dbb"
 ]
