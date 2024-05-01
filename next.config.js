f4caf7c4bc4076029263b93e8f2af751c9c95384
Pratyush Sudhakar <ps2245@cornell.edu>
2024-04-30 23:32:59 -0400

    format

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const {
-	withNextVideo
-} = require('next-video/process')
+const { withNextVideo } = require("next-video/process");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,46 +9,57 @@ const nextConfig = {
 		esmExternals: true
 	},
 	images: {
-		domains: ["i.scdn.co", "images.unsplash.com", "upload.wikimedia.org", "source.unsplash.com", "privatewebsitecontent.s3.amazonaws.com"]
+		domains: [
+			"i.scdn.co",
+			"images.unsplash.com",
+			"upload.wikimedia.org",
+			"source.unsplash.com",
+			"privatewebsitecontent.s3.amazonaws.com"
+		]
 	},
 	async headers() {
-		return [{
-			source: "/api/graphql",
-			headers: [{
-					key: "Access-Control-Allow-Credentials",
-					value: "true"
-				},
-				{
-					key: "Access-Control-Allow-Origin",
-					value: "*"
-				},
-				{
-					key: "Access-Control-Allow-Methods",
-					value: "GET,DELETE,PATCH,POST,PUT"
-				},
-				{
-					key: "Access-Control-Allow-Headers",
-					value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-				},
-			]
-		}]
+		return [
+			{
+				source: "/api/graphql",
+				headers: [
+					{
+						key: "Access-Control-Allow-Credentials",
+						value: "true"
+					},
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*"
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,DELETE,PATCH,POST,PUT"
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+					}
+				]
+			}
+		];
 	},
 	async rewrites() {
-		return [{
-			source: '/close-friends',
-			destination: 'https://private.pratyushsudhakar.com/',
-		}]
+		return [
+			{
+				source: "/close-friends",
+				destination: "https://private.pratyushsudhakar.com/"
+			}
+		];
 	},
 	webpack: (config, options) => {
 		config.module.rules.push({
 			test: /\.(graphql|gql)/,
 			exclude: /node_modules/,
 			loader: "graphql-tag/loader"
-		})
-		return config
+		});
+		return config;
 	}
 };
 
 module.exports = withNextVideo(nextConfig, {
-	folder: 'public/videos'
+	folder: "public/videos"
 });
