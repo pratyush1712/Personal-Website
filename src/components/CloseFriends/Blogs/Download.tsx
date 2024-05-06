2c7c5783965cfff28f17e73213c73a83511c897f
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2024-05-06 10:55:39 -0400

    Update Download.tsx
    Signed-off-by: Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>

--- src/components/CloseFriends/Blogs/Download.tsx
+++ src/components/CloseFriends/Blogs/Download.tsx
@@ -3,20 +3,21 @@ import { Button } from "@mui/material";
 import { sendGAEvent } from "@next/third-parties/google";
 
 export default function DownloadBlogButton({ blog }: { blog: any }) {
-	const onBlogDownload = (window: Window) => {
-		console.log("Blog downloaded");
+	const onBlogDownload = () => {
+		// console.log("Blog downloaded");
 		sendGAEvent({ event: "Blog Downloaded", value: blog.title });
-		window.close();
+		// window.close();
 	};
 	const handleDownload = async () => {
-		const preview = window.open("http://localhost:3000/preview", blog.title);
-		preview?.document.addEventListener("afterprint", event => onBlogDownload(preview!));
-		preview?.document.open();
-		if (preview) preview.document.title = blog.title;
-		preview?.document.write(blog.htmlContent);
-		preview?.focus();
-		preview?.print();
-		preview?.document.close();
+		const preview = window.open(`https://privatewebsitecontent.s3.amazonaws.com/pdf/${blog.title}.pdf`);
+		onBlogDownload();
+		// preview?.document.addEventListener("afterprint", event => onBlogDownload(preview!));
+		// preview?.document.open();
+		// if (preview) preview.document.title = blog.title;
+		// preview?.document.write(blog.htmlContent);
+		// preview?.focus();
+		// preview?.print();
+		// preview?.document.close();
 	};
 	return (
 		<Button
