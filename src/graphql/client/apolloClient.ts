548e766cf41a70e1000b15f020775814e84deee7
Pratyush Sudhakar <ps2245@cornell.edu>
2024-05-11 06:44:28 -0400

    do somethin

--- src/graphql/client/apolloClient.ts
+++ src/graphql/client/apolloClient.ts
@@ -3,10 +3,12 @@ import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rs
 
 export const { getClient } = registerApolloClient(() => {
 	return new ApolloClient({
+		ssrMode: typeof window === "undefined",
 		cache: new InMemoryCache(),
 		link: new HttpLink({
 			uri: process.env.NEXT_PUBLIC_URL_SERVER_GRAPHQL,
-			fetchOptions: { cache: "no-store" }
+			fetchOptions: { cache: "no-store" },
+			credentials: "same-origin"
 		})
 	});
 });
