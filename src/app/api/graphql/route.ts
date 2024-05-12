ec06aa60b7adefff62399ceaa57d1091d6b67a3c
Pratyush Sudhakar <ps2245@cornell.edu>
2024-05-11 21:19:29 -0400

    format error

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,13 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServerErrorCode } from "@apollo/server/errors";
 import typeDefs from "@/graphql/schema";
 import resolvers from "@/graphql/resolvers";
 import { NextApiRequest } from "next";
 import { Blogs, Videos } from "@/graphql/database/controllers";
 import { BlogModel, VideoModel } from "@/graphql/database/models";
 import mongoose from "mongoose";
-import { cookies, headers } from "next/headers";
-import { decode } from "next-auth/jwt";
 
 const uri = process.env.MONGODB_URI;
 
@@ -26,24 +25,16 @@ connectDB();
 const apolloServer = new ApolloServer({
 	typeDefs,
 	resolvers,
-	introspection: process.env.NEXT_PUBLIC_VERCEL_ENV !== "production",
-	csrfPrevention: true
+	formatError: (formattedError, error) => {
+		if (formattedError.extensions?.code === ApolloServerErrorCode.INTERNAL_SERVER_ERROR) {
+			console.error("🔥", error);
+		}
+		return formattedError;
+	}
 });
 
 const handler = startServerAndCreateNextHandler(apolloServer, {
-	context: async (req: Request) => {
-		if (req.method === "POST") {
-			const reqCookies = cookies().getAll();
-			const token = reqCookies.find(cookie => cookie.name === "next-auth.session-token")?.value;
-			const mutation = headers().get("mutation") === "true";
-			if (mutation) {
-				const decoded = await decode({ token, secret: process.env.NEXTAUTH_SECRET! });
-				if (decoded?.email! !== "pratyushsudhakar03@gmail.com") {
-					throw new Error("You are not authorized to perform this action");
-				}
-			}
-		}
-
+	context: async (req: NextApiRequest) => {
 		return {
 			dataSources: {
 				blogs: new Blogs({ modelOrCollection: BlogModel }),
