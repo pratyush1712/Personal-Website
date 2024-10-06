454645ae833ad0ef68de1364ec587b00d9c711de
Pratyush Sudhakar <ps2245@cornell.edu>
2024-10-06 19:22:42 -0400

    reduce main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Box, Divider, Grid, IconButton, Link, Stack, Tooltip, Typography } from "@mui/material";
 import { links } from "@/utils/links";
-import PlayList from "@/components/HomeLayout/PlayList";
+import Image from "next/image";
 
 export default function Home() {
 	const iconLink = (link: any) => {
@@ -14,44 +14,39 @@ export default function Home() {
 	};
 
 	return (
-		<Grid container direction="column" alignItems="center" spacing={0}>
-			<Grid item xs={12} sx={{ mt: -2, mb: 1 }}>
-				<Typography
-					variant="h3"
-					align="center"
-					sx={{
-						margin: "20px 0 0 0",
-						fontSize: { xs: "1.5rem", sm: "2.5rem" }
-					}}>
-					Pratyush Sudhakar
-				</Typography>
-				{/* <Typography
-					variant="caption"
-					align="center"
-					sx={{
-						backgroundColor: "rgba(0, 0, 0, 0.7)",
-						borderRadius: "2px",
-						color: "#FFFFFF",
-						padding: "5px",
-						fontSize: { xs: "0.4rem", sm: "0.8rem" }
-					}}>
-					I strive to be known as the most approachable person. Always here for friends, nk. call me &#128524;
-				</Typography> */}
+		<Grid container alignItems="center" justifyContent="center" spacing={0}>
+			<Grid
+				item
+				sx={{
+					mt: 20,
+					alignItems: "flex-start",
+					display: "flex",
+					flexDirection: "column"
+				}}>
+				<Grid item xs={12} sx={{ mt: 2, mb: 1 }}>
+					<Typography variant="h1" sx={{ ml: { xs: -2, sm: 0 }, fontSize: { xs: "1.5rem", sm: "3.5rem" } }}>
+						Pratyush Sudhakar
+					</Typography>
+				</Grid>
+				{/* Links at the bottom */}
+				<Grid item xs={12} sx={{ ml: { xs: -2, sm: 0 } }}>
+					<Box sx={{ minWidth: "100%", display: "flex", justifyContent: "center" }}>
+						<Stack direction="row" spacing={{ xs: 2, sm: 2.5 }} sx={{ mx: "auto" }}>
+							{links.filter(link => link.type === "professional").map(iconLink)}
+							<Divider orientation="vertical" flexItem />
+							{links.filter(link => link.type === "social").map(iconLink)}
+						</Stack>
+					</Box>
+				</Grid>
 			</Grid>
-			{/* Links at the bottom */}
-			<Grid item xs={12} sx={{ ml: { xs: -2, sm: 0 } }}>
-				<Box sx={{ minWidth: "100%", display: "flex", justifyContent: "center" }}>
-					<Stack direction="row" spacing={{ xs: 2, sm: 2.5 }} sx={{ mx: "auto" }}>
-						{links.filter(link => link.type === "professional").map(iconLink)}
-						<Divider orientation="vertical" flexItem />
-						{links.filter(link => link.type === "social").map(iconLink)}
-					</Stack>
-				</Box>
-			</Grid>
-
-			{/* Playlist occupying most of the screen */}
-			<Grid item xs={12} sx={{ flexGrow: 1, minWidth: "100%", justifyContent: "center" }}>
-				<PlayList />
+			<Grid item sx={{ mt: 23 }}>
+				<Image
+					src="/favicon.png"
+					alt="Pratyush Sudhakar"
+					width={120}
+					height={120}
+					style={{ border: 2, borderRadius: "80%" }}
+				/>
 			</Grid>
 		</Grid>
 	);
--- src/components/HomeLayout/PlayList.tsx
+++ src/components/HomeLayout/PlayList.tsx
@@ -1,319 +1,129 @@
 "use client";
-import "@/styles/playlist.css";
-import songs from "@/utils/songs.json";
 import { Box, Card, CardContent, CardMedia, Container, IconButton, Typography } from "@mui/material";
-import Image from "next/image";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import "react-bubble-ui/dist/index.css";
-import { FaSpotify } from "react-icons/fa";
-import BubbleUI from "./Play";
-
-const shuffle = (array: any[]) => {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-	return array;
-};
-
-const options = {
-	size: 180,
-	minSize: 70,
-	gutter: 20,
-	provideProps: false,
-	fringeWidth: 160,
-	yRadius: 130,
-	xRadius: 220,
-	cornerRadius: 50,
-	showGuides: false,
-	compact: true,
-	gravitation: 5
-};
-const ITEMS_PER_LOAD = 500;
-
-function PlaylistLabel() {
-	return (
-		<Container
-			sx={{
-				textAlign: "center",
-				position: "absolute",
-				zIndex: 2,
-				bottom: "0%",
-				left: { xs: "0%", sm: "5%" },
-				width: "max-content",
-				py: 2,
-				px: 0,
-				backgroundColor: "rgba(29, 185, 84, 1)",
-				alignItems: "center",
-				borderLeft: "1px solid #1db954",
-				borderBottom: "1px solid #1db954"
-			}}>
-			<Typography
-				variant="body2"
-				sx={{
-					color: "white",
-					px: { xs: 1, sm: 0 },
-					mx: 0
-				}}>
-				My Spotify Playlist. Scroll to see more songs...
-				<FaSpotify
-					style={{
-						color: "white",
-						fontSize: "1.20em",
-						marginLeft: "10px",
-						verticalAlign: "middle"
-					}}
-				/>
-			</Typography>
-		</Container>
-	);
-}
+import { useState, useEffect, useRef } from "react";
+import { FaPlay, FaPause, FaStepForward, FaStepBackward } from "react-icons/fa";
+import songs from "@/utils/songs.json";
 
 function truncateString(str: string, num: number) {
-	if (str.length <= num) {
-		return str;
-	}
-	return str.slice(0, num) + "...";
-}
-
-function PlayListListView() {
-	const shuffledSongs = useMemo(() => shuffle(songs), []);
-	return (
-		<Container
-			sx={{
-				mt: 2,
-				maxHeight: "60vh",
-				overflowY: "scroll",
-				scrollbarWidth: "thin",
-				scrollbarColor: "#1db954 #333"
-			}}>
-			{shuffledSongs.map((song, index) => (
-				<Card
-					sx={{
-						border: "1px solid #1db954",
-						display: "flex",
-						maxWidth: "100%",
-						minWidth: "100%",
-						my: 2,
-						alignItems: "center",
-						justifySelf: "center",
-						fontSize: "0.3rem",
-						maxHeight: "min-content",
-						cursor: "pointer",
-						justifyContent: "space-between"
-					}}
-					key={index}>
-					<Box sx={{ display: "flex", flexDirection: "column" }}>
-						<CardContent sx={{ flex: "1 0 auto" }}>
-							<Typography component="div" variant="subtitle2">
-								{truncateString(song.name, 30)}
-							</Typography>
-							<Typography variant="caption" color="text.secondary" component="div">
-								{truncateString(song.artist, 20)}
-							</Typography>
-						</CardContent>
-						<Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-							<IconButton aria-label="play/pause">
-								<FaSpotify />
-							</IconButton>
-						</Box>
-					</Box>
-					<CardMedia component="img" sx={{ width: 151 }} image={song.image} alt={song.name} />
-				</Card>
-			))}
-		</Container>
-	);
+	return str.length <= num ? str : str.slice(0, num) + "...";
 }
 
 export default function PlayList() {
-	const hoverRef = useRef<number>(-1);
-	const [hoverIndex, setHoverIndex] = useState<number>(-1);
-	const [hoverTimeoutId, setHoverTimeoutId] = useState<NodeJS.Timeout | undefined>();
-	// const switchRef = useRef<number>(0);
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const randomSongsOffset = useRef<number>(0);
-	const randomSongs = useMemo(() => shuffle(songs), []);
-	const [displayedSongs, setDisplayedSongs] = useState<any[]>(randomSongs.slice(0, ITEMS_PER_LOAD));
+	const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const containerRef = useRef<HTMLDivElement | null>(null);
 
-	const handleBubbleClick = useCallback((url: string | URL | undefined) => {
-		window.open(url, "_blank");
-	}, []);
+	const handlePlayPause = () => {
+		setIsPlaying(prev => !prev);
+	};
+
+	const handleNextSong = () => {
+		setCurrentSongIndex(prevIndex => (prevIndex + 1) % songs.length);
+	};
+
+	const handlePreviousSong = () => {
+		setCurrentSongIndex(prevIndex => (prevIndex - 1 + songs.length) % songs.length);
+	};
 
 	useEffect(() => {
-		setDisplayedSongs(randomSongs.slice(0, ITEMS_PER_LOAD));
-	}, [randomSongs]);
-
-	const loadMore = useCallback(
-		(prev: any) => {
-			randomSongsOffset.current += ITEMS_PER_LOAD;
-			if (randomSongsOffset.current >= randomSongs.length) {
-				randomSongsOffset.current = 0;
-				return [...prev.slice(ITEMS_PER_LOAD, prev.length), ...shuffle(prev.slice(0, ITEMS_PER_LOAD))];
-			}
-			const newSongs = randomSongs.slice(randomSongsOffset.current, randomSongsOffset.current + ITEMS_PER_LOAD);
-			return [
-				...prev,
-				...newSongs.map((song, index) => {
-					if (!song || !song.image) {
-						console.log("Invalid song", song);
-						return null;
-					}
-					return (
-						<div
-							key={index}
-							className={`bubble ${hoverIndex === index ? "bubble-hover" : ""}`}
-							style={{
-								width: options.size,
-								height: options.size,
-								display: "flex",
-								alignItems: "center",
-								justifyContent: "center",
-								transition: "all 0.5s ease",
-								cursor: "pointer",
-								position: "relative"
-							}}
-							onMouseEnter={() => {
-								const timeoutId = setTimeout(() => {
-									setHoverIndex(index);
-									hoverRef.current = index;
-								}, 600);
-								setHoverTimeoutId(timeoutId);
-							}}
-							onMouseLeave={() => {
-								if (hoverTimeoutId) clearTimeout(hoverTimeoutId);
-								setHoverIndex(-1);
-								hoverRef.current = -1;
-							}}
-							onClick={() => handleBubbleClick(`https://open.spotify.com/track/${song?.id}`)}>
-							<Image
-								loading="lazy"
-								onError={e => (e.currentTarget.src = "/images/default.jpg")}
-								src={song?.image}
-								alt={song?.name}
-								width={options.size}
-								height={options.size}
-								style={{
-									borderRadius: "50%",
-									transition: "transform 0.3s ease",
-									border: "3px solid rgba(29, 185,84, 0.8)"
-								}}
-							/>
-							<Typography
-								variant="subtitle2"
-								sx={{
-									position: "absolute",
-									paddingLeft: "auto",
-									paddingRight: "auto",
-									color: "#FFFFFF",
-									height: "auto",
-									backgroundColor: "rgba(30, 30, 30, 0.9)",
-									textAlign: "center",
-									left: 0
-								}}>
-								{song?.name} By: {song?.artist}
-							</Typography>
-						</div>
-					);
-				})
-			];
-		},
-		[randomSongs, handleBubbleClick, hoverIndex, hoverTimeoutId]
-	);
-
-	// const loadMoreSongs = useCallback(() => {
-	// 	setDisplayedSongs(prevSongs => {
-	// 		const newSongs = randomSongs.slice(randomSongsOffset.current, randomSongsOffset.current + ITEMS_PER_LOAD);
-	// 		randomSongsOffset.current += ITEMS_PER_LOAD;
-	// 		return prevSongs.map((song, index) => (index % 2 === switchRef.current ? newSongs[index] : song));
-	// 	});
-	// 	switchRef.current = switchRef.current === 0 ? 1 : 0;
-	// }, [randomSongs]);
-
-	// useEffect(() => {
-	// 	const intervalId = setInterval(() => {
-	// 		if (hoverRef.current === -1) loadMoreSongs();
-	// 	}, 7000);
-
-	// 	return () => clearInterval(intervalId);
-	// }, [loadMoreSongs]);
-
-	const songBubbles = useMemo(
-		() =>
-			displayedSongs.map((song, index) => {
-				if (!song || !song.image) {
-					console.log("Invalid song", song);
-					return null;
-				}
-				return (
-					<div
-						key={index}
-						className={`bubble ${hoverIndex === index ? "bubble-hover" : ""}`}
-						style={{
-							width: options.size,
-							height: options.size,
-							display: "flex",
-							position: "relative",
-							alignItems: "center",
-							justifyContent: "center",
-							transition: "all 0.5s ease",
-							cursor: "pointer"
-						}}
-						onMouseEnter={() => {
-							const timeoutId = setTimeout(() => {
-								setHoverIndex(index);
-								hoverRef.current = index;
-							}, 600);
-							setHoverTimeoutId(timeoutId);
-						}}
-						onMouseLeave={() => {
-							if (hoverTimeoutId) clearTimeout(hoverTimeoutId);
-							setHoverIndex(-1);
-							hoverRef.current = -1;
-						}}
-						onClick={() => handleBubbleClick(`https://open.spotify.com/track/${song?.id}`)}>
-						<Image
-							loading="lazy"
-							onError={e => (e.currentTarget.src = "/images/default.jpg")}
-							src={song?.image}
-							alt={song?.name}
-							width={options.size}
-							height={options.size}
-							style={{
-								borderRadius: "50%",
-								transition: "transform 0.3s ease",
-								border: "3px solid rgba(29, 185,84, 0.8)"
-							}}
-						/>
-						<Typography
-							variant="subtitle2"
-							sx={{
-								position: "absolute",
-								paddingLeft: "auto",
-								paddingRight: "auto",
-								height: "auto",
-								backgroundColor: "rgba(30, 30, 30, 0.9)",
-								textAlign: "center",
-								color: "#FFFFFF",
-								left: 0
-							}}>
-							{song?.name} By: {song?.artist}
-						</Typography>
-
-						<div className="tooltip" style={{ zIndex: 15 }}>
-							{song?.name} By: {song?.artist}
-						</div>
-					</div>
-				);
-			}),
-		[displayedSongs, hoverIndex, handleBubbleClick, hoverTimeoutId]
-	);
+		if (containerRef.current) {
+			const selectedCard = containerRef.current.children[currentSongIndex] as HTMLElement;
+			containerRef.current.scrollTo({
+				left: selectedCard.offsetLeft - containerRef.current.clientWidth / 2 + selectedCard.clientWidth / 2,
+				behavior: "smooth"
+			});
+		}
+	}, [currentSongIndex]);
 
 	return (
-		<Container sx={{ position: "relative", maxHeight: "70%" }}>
-			<PlaylistLabel />
-			<BubbleUI options={options} className="myBubbleUI" loadMore={loadMore}>
-				{songBubbles}
-			</BubbleUI>
+		<Container
+			ref={containerRef}
+			sx={{
+				mt: 4,
+				minWidth: "100%",
+				display: "flex",
+				alignItems: "center",
+				position: "relative",
+				overflowX: "scroll",
+				scrollbarWidth: "none",
+				"&::-webkit-scrollbar": { display: "none" },
+				transition: "padding-left 0.3s ease"
+			}}>
+			{songs.map((song, index) => {
+				let size = 0.6;
+				if (index === currentSongIndex) size = 1;
+				else if (index === currentSongIndex - 1 || index === currentSongIndex + 1) size = 0.8;
+				else if (index === currentSongIndex - 2 || index === currentSongIndex + 2) size = 0.7;
+
+				return (
+					<Box
+						key={index}
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							mx: 2,
+							transition: "transform 0.3s ease",
+							transform: `scale(${size})`,
+							minWidth: `${size * 200}px`
+						}}>
+						<Card
+							sx={{
+								border: currentSongIndex === index ? "2px solid #1db954" : "1px solid #444",
+								borderRadius: "50%",
+								cursor: "pointer",
+								minWidth: `${size * 200}px`,
+								minHeight: `${size * 200}px`
+							}}
+							onClick={() => setCurrentSongIndex(index)}>
+							<CardMedia
+								component="img"
+								sx={{ borderRadius: "50%" }}
+								image={song.image}
+								alt={song.name}
+							/>
+						</Card>
+						<CardContent sx={{ textAlign: "center", minWidth: "180%" }}>
+							<Typography variant="subtitle2" sx={{ fontWeight: "bold", color: "#fff" }}>
+								{truncateString(song.name, 20)}
+							</Typography>
+							<Typography variant="caption" sx={{ color: "#bbb" }}>
+								{truncateString(song.artist, 20)}
+							</Typography>
+						</CardContent>
+					</Box>
+				);
+			})}
+			{currentSongIndex !== null && (
+				<Box
+					sx={{
+						position: "absolute",
+						top: "100%",
+						minHeight: "fit-content",
+						left: "22%",
+						width: "fit-content",
+						display: "flex",
+						alignItems: "center",
+						backgroundColor: "#282828",
+						minWidth: "30vw",
+						borderRadius: "10px",
+						px: 4,
+						py: 2,
+						zIndex: 999,
+						boxShadow: "0 4px 10px rgba(0,0,0,0.5)",
+						justifyContent: "space-between"
+					}}>
+					<IconButton onClick={handlePreviousSong} sx={{ color: "#1db954" }}>
+						<FaStepBackward />
+					</IconButton>
+					<IconButton onClick={handlePlayPause} sx={{ color: "#1db954", mx: 1 }}>
+						{isPlaying ? <FaPause /> : <FaPlay />}
+					</IconButton>
+					<IconButton onClick={handleNextSong} sx={{ color: "#1db954" }}>
+						<FaStepForward />
+					</IconButton>
+				</Box>
+			)}
 		</Container>
 	);
 }
