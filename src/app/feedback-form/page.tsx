91998754dd0d8f4861d2bf13a62e06b0aed1da34
Pratyush Sudhakar <ps2245@cornell.edu>
2024-11-20 02:08:10 -0500

    affiliations

---
+++ src/app/feedback-form/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Loading from "@/ui/Loading";
+import { Container } from "@mui/material";
+
+const fetchLocation = async (router: any) => {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_RESTDB_URL!}/location`, {
+		method: "GET",
+		headers: {
+			"cache-control": "no-cache",
+			"Content-Type": "application/json",
+			"x-apikey": process.env.NEXT_PUBLIC_RESTDB_KEY!
+		}
+	});
+	const data = await res.json();
+	await getClientLocation();
+	if (data.length <= 2) {
+		router.push("/?feedback=true");
+	} else {
+		window.location.href = process.env.NEXT_PUBLIC_FEEDBACK_FORM_URL!;
+	}
+};
+
+const getClientLocation = async () => {
+	const res = await fetch("https://ipinfo.io/json");
+	const locationData = await res.json();
+	await fetch(`${process.env.NEXT_PUBLIC_RESTDB_URL!}/location`, {
+		method: "POST",
+		headers: {
+			"cache-control": "no-cache",
+			"Content-Type": "application/json",
+			"x-apikey": "673d7d6e126911526217aacb"
+		},
+		body: JSON.stringify(locationData)
+	});
+};
+
+export default function RedirectToHome() {
+	const router = useRouter();
+
+	useEffect(() => {
+		fetchLocation(router);
+	}, [router]);
+
+	return (
+		<Container
+			sx={{
+				zIndex: 1000,
+				background: "#1e1e1e",
+				color: "#1e1e1e",
+				backgroundColor: "#1e1e1e",
+				width: "100vw",
+				position: "fixed",
+				left: 0,
+				top: 0,
+				height: "100vh"
+			}}>
+			<Loading />
+		</Container>
+	);
+}
